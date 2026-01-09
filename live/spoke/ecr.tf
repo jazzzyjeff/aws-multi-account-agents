4c4efedbc50d7b4743800bdd7f471eb83157a05b
jazzytoor <32848916+jazzytoor@users.noreply.github.com>
2026-01-09 08:23:05 +0000

    chore: put ado resources into one

--- live/spoke/ecr.tf
+++ live/spoke/ecr.tf
@@ -24,3 +24,16 @@ module "ecr" {
 
   tags = local.default_tags
 }
+
+resource "docker_image" "ado" {
+  name = "${module.ecr.repository_url}:latest"
+  build {
+    context    = "${path.module}/workloads/ado-agent/docker"
+    dockerfile = "${path.module}/workloads/ado-agent/docker/Dockerfile"
+  }
+  platform = "linux/arm64"
+}
+
+resource "docker_registry_image" "ado" {
+  name = docker_image.ado.name
+}
